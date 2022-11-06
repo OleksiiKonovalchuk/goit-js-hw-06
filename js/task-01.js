193aebaaab963ed58cc1db5b076f5ed439c4697c
OleksiiKonovalchuk <108429819+OleksiiKonovalchuk@users.noreply.github.com>
2022-11-06 22:26:21 +0200

    done (task 1)

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,20 @@
 const categories = document.querySelectorAll(".item");
 console.log(`Number of categories: ${categories.length}`);
 
-function showCategory(index)  {
-  const category = categories[index];
-  console.log(`Category: ${category.firstElementChild.textContent}`);
-  console.log(`Elements: ${category.lastElementChild.children.length}`);
-}
-showCategory(0);
-showCategory(1)
-showCategory(2)
+// function showCategory(index) {
+// 	const category = categories[index];
+// 	console.log(`Category: ${category.firstElementChild.textContent}`);
+// 	console.log(`Elements: ${category.lastElementChild.children.length}`);
+// }
+// showCategory(0);
+// showCategory(1);
+// showCategory(2);
+
+const showCategory2 = array => {
+	for (const item of array) {
+		console.log(`Category: ${item.firstElementChild.textContent}`);
+		console.log(`Elements: ${item.lastElementChild.children.length}`);
+	}
+};
+
+showCategory2(categories);
